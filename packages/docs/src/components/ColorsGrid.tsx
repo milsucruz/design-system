10db88842ec025c413df6d03264df5643be62755
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-07-03 17:44:14 -0300

    change repo name

--- packages/docs/src/components/ColorsGrid.tsx
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -1,4 +1,4 @@
-import { colors } from '@ignite-ui/tokens'
+import { colors } from '@ignite-ui-milsu/tokens'
 import { getContrast } from 'polished'
 
 export function ColorsGrid() {
--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Button, ButtonProps } from '@ignite-ui/react'
+import { Button, ButtonProps } from '@ignite-ui-milsu/react'
 import { ArrowRight } from 'phosphor-react'
 
 export default {
