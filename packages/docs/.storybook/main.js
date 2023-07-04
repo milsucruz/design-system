5bd10050c3033265699b68f756fffae78e664699
Edmilson Cruz <edmilsoncruz.dev@gmail.com>
2023-07-04 09:02:01 -0300

    Storybook main.js

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,27 +1,54 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
+// /** @type { import('@storybook/react-vite').StorybookConfig } */
+// const config = {
+//   "stories": [
+//     "../src/pages/**/*.stories.mdx",
+//     "../src/stories/**/*.stories.tsx"
+//   ],
+//   addons: [
+//     "@storybook/addon-links",
+//     "@storybook/addon-essentials",
+//     "@storybook/addon-interactions",
+//     "@storybook/addon-a11y",
+//   ],
+//   framework: {
+//     name: "@storybook/react-vite",
+//     options: {},
+//   },
+//   docs: {
+//     autodocs: true,
+//   },
+//   viteFinal: (config, {configType}) => {
+//     if(configType === 'PRODUCTION') {
+//       config.base = '/design-system/'
+//     }
+//     return config
+//   }
+// };
+// export default config;
+
+module.exports = {
   "stories": [
     "../src/pages/**/*.stories.mdx",
     "../src/stories/**/*.stories.tsx"
   ],
-  addons: [
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    "@storybook/addon-a11y"
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
-  docs: {
-    autodocs: true,
+  "features": {
+    "storyStoreV7": true
   },
-  viteFinal: (config, {configType}) => {
-    if(configType === 'PRODUCTION') {
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
       config.base = '/design-system/'
     }
+
     return config
   }
-};
-export default config;
+}
